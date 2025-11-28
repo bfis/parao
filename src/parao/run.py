706d50fb50563980e934d5690b3dc8c07a552483
Benjamin Fischer <benjamin.fischer@cern.ch>
2025-11-28 12:51:54 +0100

    run: rework Runner integration, fixes ConcurrentRunner

--- src/parao/run.py
+++ src/parao/run.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 from collections.abc import Callable, Iterable
-from concurrent.futures import Executor, wait
-from typing import TYPE_CHECKING, Self, overload
+from concurrent.futures import Executor, Future
+from graphlib import TopologicalSorter
+from queue import Empty, SimpleQueue
+from typing import TYPE_CHECKING, Literal, Self, overload
 
 from .action import RecursiveAct, _RecursiveAction
 from .cast import Opaque
@@ -42,23 +44,18 @@ class _RunAct[R, A: _RunAction, I: ParaO](RecursiveAct[R, A, I]):
     __call__: Callable[[], R]
 
     def _func(
-        self, sub: Iterable[Self], runner: "_Runner[R] | None" = None, **kwargs
+        self, sub: Iterable[Self], runner: "Runner[R] | None" = None, **kwargs
     ) -> R:
         if runner is None:
-            runner = _Runner.current()
-        if runner is None:
-            if self.done:
-                return self.output.load()
-            else:
-                for s in sub:
-                    if not s.done:
-                        s(**kwargs)
+            runner = Runner.current()
+        return runner(self, sub, kwargs)
 
-                return self._make()
-        else:
-            return runner(self, sub, kwargs)
+    @overload
+    def produce(self, output: Literal[False]) -> None: ...
+    @overload
+    def produce(self, output: bool = True) -> R: ...
 
-    def _make(self, output: bool = True):
+    def produce(self, output: bool = True):
         out = self.output.dump(self.action.func(self.instance))
         if output:
             return out
@@ -98,21 +95,76 @@ class _RunAction[R](_RecursiveAction[R, []]):
 _RunAction._peer_base = _RunAction
 
 
-class _Runner[R](ABC):
-    current = ContextValue["_Runner | None"]("currentRunner", default=None)
+class Runner[R](ABC):
+    __slots__ = ()
+    current: ContextValue[Self]
+
+    def __call__(self, act: _RunAct, sub: Iterable[_RunAct], aux: dict) -> R:
+        if act.done:
+            return act.output.load()
+        else:
+            for s in sub:
+                if not s.done:
+                    s(**aux)
+            return act.produce()
+
+    def scoped_produce(self, act: _RunAct, return_output: bool = True):
+        with self.current(self):
+            return act.produce(output=return_output)
+
+
+Runner.current = ContextValue[Runner]("currentRunner", default=Runner())
+
+
+class _TopoRunner[R](Runner[R]):
+    __slots__ = ()
+    _cls: type[TopologicalSorter] = TopologicalSorter
+
+    def __call__(self, act, sub, aux):
+        if act.done:
+            assert "topo" not in aux
+            return act.output.load()
+
+        if root := "topo" not in aux:
+            aux["topo"] = self._cls()
+        aux["runner"] = self
+        topo: TopologicalSorter[_RunAct] = aux["topo"]
+
+        for t in (todo := [s for s in sub if not s.done]):
+            t(**aux)
+
+        topo.add(act, *todo)
+        if root:
+            return self.resolve(topo, act)
 
     @abstractmethod
-    def __call__(self, act: _RunAct, sub: Iterable[_RunAct], sub_kwargs: dict) -> R: ...
+    def resolve(self, topo: TopologicalSorter[_RunAct], root: _RunAct) -> R: ...
 
 
-class ConcurrentRunner(_Runner):
+class ConcurrentRunner(_TopoRunner):
     def __init__(self, executor: Executor):
         self.executor = executor
         super().__init__()
 
-    def __call__(self, act: _RunAct, sub: Iterable[_RunAct], sub_kwargs: dict):
-        if act.done:
-            return act.output.load()
-        else:
-            wait(self.executor.submit(s, **sub_kwargs) for s in sub if not s.done)
-            return self.executor.submit(act._make).result()
+    def resolve(self, topo, root):
+        run = Runner().scoped_produce
+        waiting: dict[Future, _RunAct] = {}
+        queue = SimpleQueue[_RunAct]()
+        topo.prepare()
+        while topo.is_active():
+            for act in topo.get_ready():
+                future = self.executor.submit(run, act, act is root)
+                future.add_done_callback(queue.put_nowait)
+                waiting[future] = act
+            wait = True
+            while True:
+                try:
+                    future = queue.get(block=wait)
+                except Empty:
+                    break
+                act = waiting.pop(future)
+                result = future.result()
+                topo.done(act)
+                wait = False
+        assert root is act
+        return result
